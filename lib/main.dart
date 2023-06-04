1595825065259b98e51b36c9b76b21b06e8f5b15
Ahmad <ahmadabdhaleem@gmail.com>
2023-06-04 15:05:33 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+/*     SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]); */
     return const MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
@@ -43,86 +47,19 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: const Text('Piano'),
         centerTitle: true,
       ),
       body: InteractivePiano(
         keyWidth: 60,
         noteRange: NoteRange.forClefs([
+          Clef.Bass,
+          Clef.Alto,
           Clef.Treble,
         ]),
         onNotePositionTapped: (position) {
-          if (position == NotePosition(note: Note.C, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 60);
-          } else if (position ==
-              NotePosition(
-                  note: Note.C, octave: 4, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 61);
-          } else if (position == NotePosition(note: Note.D, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 62);
-          } else if (position ==
-              NotePosition(
-                  note: Note.D, octave: 4, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 63);
-          } else if (position == NotePosition(note: Note.E, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 64);
-          } else if (position == NotePosition(note: Note.F, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 65);
-          } else if (position ==
-              NotePosition(
-                  note: Note.F, octave: 4, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 66);
-          } else if (position == NotePosition(note: Note.G, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 67);
-          } else if (position ==
-              NotePosition(
-                  note: Note.G, octave: 4, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 68);
-          } else if (position == NotePosition(note: Note.A, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 69);
-          } else if (position ==
-              NotePosition(
-                  note: Note.A, octave: 4, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 70);
-          } else if (position == NotePosition(note: Note.B, octave: 4)) {
-            FlutterMidi().playMidiNote(midi: 71);
-          } else if (position == NotePosition(note: Note.C, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 72);
-          } else if (position ==
-              NotePosition(
-                  note: Note.C, octave: 5, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 73);
-          } else if (position == NotePosition(note: Note.D, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 74);
-          } else if (position ==
-              NotePosition(
-                  note: Note.D, octave: 5, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 75);
-          } else if (position == NotePosition(note: Note.E, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 76);
-          } else if (position == NotePosition(note: Note.F, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 77);
-          } else if (position ==
-              NotePosition(
-                  note: Note.F, octave: 5, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 78);
-          } else if (position == NotePosition(note: Note.G, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 79);
-          } else if (position ==
-              NotePosition(
-                  note: Note.G, octave: 5, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 80);
-          } else if (position == NotePosition(note: Note.A, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 81);
-          } else if (position ==
-              NotePosition(
-                  note: Note.A, octave: 5, accidental: Accidental.Sharp)) {
-            FlutterMidi().playMidiNote(midi: 82);
-          } else if (position == NotePosition(note: Note.B, octave: 5)) {
-            FlutterMidi().playMidiNote(midi: 83);
-          } else if (position == NotePosition(note: Note.C, octave: 6)) {
-            FlutterMidi().playMidiNote(midi: 84);
-          }
+          FlutterMidi().playMidiNote(midi: position.pitch);
         },
       ),
     );
